3eacd68ff42635f3afd083bc223a64bc7a4d7e7e
CheapeN1 <134295484+CheapeN1@users.noreply.github.com>
2024-09-11 20:17:57 +0300

    form kısmı işlevsel hale getirildi ve veritabanına bağlanıldı.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Posts = require('./models/post');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost:27017/cleanblog-test-db' , {
+  useNewUrlParser : true,
+  useUnifiedTopology:true,
+
+})
 
 //TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Posts.find({})
+  res.render('index', {
+    posts
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -20,7 +34,10 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-
+app.post('/posts', async  (req, res) => {
+  await Posts.create(req.body)
+  res.redirect('/');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
